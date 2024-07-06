6790a51e14c176ce6656a9b520bbbb3b0447112b
Fufutis <fufutis15@gmail.com>
2024-07-06 22:45:29 +0300

    Extra details for items

--- fetchData.js
+++ fetchData.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {//after html is loaded this function is called
+document.addEventListener("DOMContentLoaded", () => {
     const fetchChampionButton = document.getElementById('fetchChampion');
     const championNameInput = document.getElementById('championName');
     const fetchItemButton = document.getElementById('fetchItem');
@@ -6,21 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {//after html is loaded this
     const resultCDiv = document.getElementById('resultC');
     const resultItemDiv = document.getElementById('resultItems');
 
-    fetchChampionButton.addEventListener('click', () => {//The BUTTON is clicked
+    fetchChampionButton.addEventListener('click', () => {
         const championName = championNameInput.value.trim();
         if (championName) {
             fetchChampionStats(championName);
         }
     });
 
-    fetchItemButton.addEventListener('click', () => {//The BUTTON is clicked
+    fetchItemButton.addEventListener('click', () => {
         const itemName = itemNameInput.value.trim().toLowerCase();
         if (itemName) {
             fetchItemStats(itemName);
         }
     });
 
-    async function fetchChampionStats(championName) {//The DATA is preped here
+    async function fetchChampionStats(championName) {
         try {
             const response = await fetch(`https://ddragon.leagueoflegends.com/cdn/14.12.1/data/en_US/champion/${championName}.json`);
             if (!response.ok) {
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {//after html is loaded this
         }
     }
 
-    async function fetchItemStats(itemName) {//The DATA is preped here
+    async function fetchItemStats(itemName) {
         try {
             const response = await fetch(`https://ddragon.leagueoflegends.com/cdn/14.12.1/data/en_US/item.json`);
             if (!response.ok) {
@@ -52,83 +52,121 @@ document.addEventListener("DOMContentLoaded", () => {//after html is loaded this
             resultItemDiv.innerHTML = `<p>${error.message}</p>`;
         }
     }
-const statMapping = {
-    "hp": 2.67,         // HP
-    "mp": 1.4,         // MP
-    "movespeed": 12,    // MS
-    "armor": 20,        // Armor
-    "spellblock": 18,   // MR
-    "attackrange": 0,   // No gold value specified for AttackRange
-    "hpregen": 3,       // HPR
-    "mpregen": 5,       // MPR
-    "crit": 40,         // CritChance
-    "attackdamage": 35, // AD
-    "attackspeed": 30   // AS
-};//lack of ap bcs no champ has any
 
-function displayChampionStats(champion) { 
-    const stats = champion.stats;
-    let totalGoldValueWithMSAndMP = 0;
-    let totalGoldValueWithoutMS = 0;
-    let totalGoldValueWithoutMSAndMP = 0;
+    const statMapping = {
+        "hp": 2.67,
+        "mp": 1.4,
+        "movespeed": 12,
+        "armor": 20,
+        "spellblock": 18,
+        "attackrange": 0,
+        "hpregen": 3,
+        "mpregen": 5,
+        "crit": 40,
+        "attackdamage": 35,
+        "attackspeed": 30
+    };
 
-    for (const [stat, value] of Object.entries(stats)) {
-        if (statMapping.hasOwnProperty(stat)) {
-            totalGoldValueWithMSAndMP += value * statMapping[stat];
-            if (stat !== "movespeed") {
-                totalGoldValueWithoutMS += value * statMapping[stat];
-            }
-            if (stat !== "movespeed" && stat !== "mp" && stat !== "mpregen") {
-                totalGoldValueWithoutMSAndMP += value * statMapping[stat];
+    function displayChampionStats(champion) {
+        const stats = champion.stats;
+        let totalGoldValueWithMSAndMP = 0;
+        let totalGoldValueWithoutMS = 0;
+        let totalGoldValueWithoutMSAndMP = 0;
+
+        for (const [stat, value] of Object.entries(stats)) {
+            if (statMapping.hasOwnProperty(stat)) {
+                totalGoldValueWithMSAndMP += value * statMapping[stat];
+                if (stat !== "movespeed") {
+                    totalGoldValueWithoutMS += value * statMapping[stat];
+                }
+                if (stat !== "movespeed" && stat !== "mp" && stat !== "mpregen") {
+                    totalGoldValueWithoutMSAndMP += value * statMapping[stat];
+                }
             }
         }
+        totalGoldValueWithMSAndMP = totalGoldValueWithMSAndMP.toFixed(1);
+        totalGoldValueWithoutMS = totalGoldValueWithoutMS.toFixed(1);
+        totalGoldValueWithoutMSAndMP = totalGoldValueWithoutMSAndMP.toFixed(1);
+
+        resultCDiv.innerHTML = '';
+        let champDiv = document.createElement('div');
+        champDiv.className = 'champion';
+        champDiv.innerHTML = `<h1>${champion.name}</h1>
+                              <p>${champion.title}</p>
+                              HP: ${stats.hp}<br>
+                              HP per Level: ${stats.hpperlevel}<br>
+                              MP: ${stats.mp}<br>
+                              MP per Level: ${stats.mpperlevel}<br>
+                              Move Speed: ${stats.movespeed}<br>
+                              Armor: ${stats.armor}<br>
+                              Armor per Level: ${stats.armorperlevel}<br>
+                              Spell Block: ${stats.spellblock}<br>
+                              Spell Block per Level: ${stats.spellblockperlevel}<br>
+                              Attack Range: ${stats.attackrange}<br>
+                              HP Regen: ${stats.hpregen}<br>
+                              HP Regen per Level: ${stats.hpregenperlevel}<br>
+                              MP Regen: ${stats.mpregen}<br>
+                              MP Regen per Level: ${stats.mpregenperlevel}<br>
+                              Crit: ${stats.crit}<br>
+                              Crit per Level: ${stats.critperlevel}<br>
+                              Attack Damage: ${stats.attackdamage}<br>
+                              Attack Damage per Level: ${stats.attackdamageperlevel}<br>
+                              Attack Speed per Level: ${stats.attackspeedperlevel}<br>
+                              Attack Speed: ${stats.attackspeed}<br>
+                              <br>
+                              Total Gold Value Level 1 : ${totalGoldValueWithMSAndMP}<br>
+                              Total Gold Value Level 1 (without MS): ${totalGoldValueWithoutMS}<br>
+                              Total Gold Value Level 1 (without MS and MP): ${totalGoldValueWithoutMSAndMP}`;
+
+        resultCDiv.appendChild(champDiv);
+
+        // Fetch and display abilities
+        displayChampionAbilities(champion.passive, champion.spells);
     }
-    // Format the gold values to one decimal place
-    totalGoldValueWithMSAndMP = totalGoldValueWithMSAndMP.toFixed(1);
-    totalGoldValueWithoutMS = totalGoldValueWithoutMS.toFixed(1);
-    totalGoldValueWithoutMSAndMP = totalGoldValueWithoutMSAndMP.toFixed(1);
 
-    resultCDiv.innerHTML = '';
-    let champDiv = document.createElement('div');
-    champDiv.className = 'champion';
-    champDiv.innerHTML = `<h1>${champion.name}</h1>
-                          <p>${champion.title}</p>
-                          HP: ${stats.hp}<br>
-                          HP per Level: ${stats.hpperlevel}<br>
-                          MP: ${stats.mp}<br>
-                          MP per Level: ${stats.mpperlevel}<br>
-                          Move Speed: ${stats.movespeed}<br>
-                          Armor: ${stats.armor}<br>
-                          Armor per Level: ${stats.armorperlevel}<br>
-                          Spell Block: ${stats.spellblock}<br>
-                          Spell Block per Level: ${stats.spellblockperlevel}<br>
-                          Attack Range: ${stats.attackrange}<br>
-                          HP Regen: ${stats.hpregen}<br>
-                          HP Regen per Level: ${stats.hpregenperlevel}<br>
-                          MP Regen: ${stats.mpregen}<br>
-                          MP Regen per Level: ${stats.mpregenperlevel}<br>
-                          Crit: ${stats.crit}<br>
-                          Crit per Level: ${stats.critperlevel}<br>
-                          Attack Damage: ${stats.attackdamage}<br>
-                          Attack Damage per Level: ${stats.attackdamageperlevel}<br>
-                          Attack Speed per Level: ${stats.attackspeedperlevel}<br>
-                          Attack Speed: ${stats.attackspeed}<br>
-                          <br>
-                          Total Gold Value Level 1 : ${totalGoldValueWithMSAndMP}<br>
-                          Total Gold Value Level 1 (without MS): ${totalGoldValueWithoutMS}<br>
-                          Total Gold Value Level 1 (without MS and MP): ${totalGoldValueWithoutMSAndMP}`;
+    function displayChampionAbilities(passive, abilities) {
+        let abilitiesDiv = document.createElement('div');
+        abilitiesDiv.className = 'abilities';
+        abilitiesDiv.innerHTML = '<h2>Abilities</h2>';
 
-    resultCDiv.appendChild(champDiv);
-}
-    function displayItemStats(item) {//this is the output
+        let passiveDiv = document.createElement('div');
+        passiveDiv.className = 'ability passive';
+        passiveDiv.innerHTML = `<strong>Passive - ${passive.name}</strong>: ${passive.description}<br>`;
+        abilitiesDiv.appendChild(passiveDiv);
+
+        abilities.forEach(ability => {
+            let abilityDiv = document.createElement('div');
+            abilityDiv.className = 'ability';
+            abilityDiv.innerHTML = `<strong>${ability.name}</strong>: ${ability.description}<br>
+                                    Cooldown: ${ability.cooldownBurn}<br>
+                                    Cost: ${ability.costBurn}<br>`;
+            abilitiesDiv.appendChild(abilityDiv);
+        });
+
+        resultCDiv.appendChild(abilitiesDiv);
+    }
+
+    function displayItemStats(item) {
         resultItemDiv.innerHTML = '';
         let itemDiv = document.createElement('div');
         itemDiv.className = 'item';
         itemDiv.innerHTML = `<h1>${item.name}</h1>
-                            <br>
-                            ${item.gold.total} Gold 
-                            <p>${item.description}</p>`;
+                            <p>${item.plaintext || 'No plain text available'}</p>
+                            <p>${item.description || 'No description available'}</p>
+                            <p>Colloquial: ${item.colloq || 'N/A'}</p>
+                            <p>Builds into: ${item.into ? item.into.join(', ') : 'N/A'}</p>
+                            <p>Image: ${item.image ? `<img src="https://ddragon.leagueoflegends.com/cdn/14.12.1/img/item/${item.image.full}" alt="${item.name}">` : 'No image available'}</p>
+                            <p>Gold: Base - ${item.gold.base}, Total - ${item.gold.total}, Sell - ${item.gold.sell}</p>
+                            <p>Tags: ${item.tags ? item.tags.join(', ') : 'N/A'}</p>
+                            
+                            <p>Stats: ${item.stats ? Object.entries(item.stats).map(([key, value]) => `${key}: ${value}`).join(', ') : 'N/A'}</p>
+                            <p>Effects: ${item.effect ? Object.entries(item.effect).map(([key, value]) => `${key}: ${value}`).join(', ') : 'N/A'}</p>
+                            `
+                            //<p>Maps: ${item.maps ? Object.keys(item.maps).filter(key => item.maps[key]).join(', ') : 'N/A'}</p>
+                            //<p>In Store: ${item.inStore ? 'Yes' : 'No'}</p>
+                            //<p>Depth: ${item.depth || 'N/A'}</p>
+                            //<p>Stacks: ${item.stacks || 'N/A'}</p>;
+
         resultItemDiv.appendChild(itemDiv);
     }
 });
-//lack of internet connection or the api shutting down you will receive failed to fetch as a responce
